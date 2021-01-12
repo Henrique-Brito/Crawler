582b0b93c84f3a4b78ed1817d5e2f5487d16011d
Henrique Brito <henriquejbrito@gmail.com>
2021-01-12 18:37:26 -0300

    Almost done

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,36 @@
 #include <set>
 #include <unistd.h>
 #include <fstream>
+#include <ctime>
 
 struct data{
-	int id;
 	std::vector<std::pair<int, std::string>> url;
 	data(){}
 };
 
+struct statistics{
+	std::string website;
+	int number;
+	double average_size;
+	double average_time;
+	statistics(){};
+	statistics(std::string s, int n, int as, double at){
+		website = s;
+		number = n;
+		average_size = ((double)as)/n;
+		average_time = ((double)at)/n;
+	}
+};
+
 const int limit = 1000;
 
-std::vector<std::string> all_links;
 std::vector<std::set<std::string>> new_links;
+
+std::vector<int> qnt_crawled;
+std::vector<int> size_html;
+std::vector<double> time_crawling;
+
+std::vector<statistics> to_output;
 
 const bool debug = false;
 
@@ -37,8 +56,12 @@ std::string collect_html(std::string url){
 const std::string file_prefix="data/website";
 const std::string file_sufix=".html";
 
+pthread_mutex_t mutex;
+
+int total_crawled=0; 
+
 int file_counter=0;
-void output_html(std::string html){
+int output_html(std::string html){
 	
 	std::ofstream out(file_prefix+std::to_string(file_counter)+file_sufix);
 	file_counter++;
@@ -48,6 +71,8 @@ void output_html(std::string html){
 	}else{
 		std::cout << "Error: cant open write file - HTML" << std::endl;
 	}
+
+	return (int)html.size();
 }
 
 void crawl(int id, std::string url){
@@ -55,13 +80,27 @@ void crawl(int id, std::string url){
 	CkSpider spider;
 	spider.Initialize(url.c_str());
 
+	usleep(100000);
 	spider.CrawlNext();
 	output_html(spider.lastHtml());
 
 	if( debug ) std::cout << "Initial string: " << url << std::endl;
 
 	for( int i=0; i<spider.get_NumUnspidered(); i++ ){
-		output_html(std::string(spider.getUnspideredUrl(i)));
+		usleep(100000);
+
+		pthread_mutex_lock(&mutex);
+
+		clock_t begin = clock();
+
+		size_html[id] += output_html(collect_html(std::string(spider.getUnspideredUrl(i))));
+
+		clock_t end = clock();
+
+		pthread_mutex_unlock(&mutex);
+		
+		time_crawling[id] += double(end-begin) / CLOCKS_PER_SEC;
+		qnt_crawled[id]++;
 		if( false ) std::cout << "Link " << i << ": " << spider.getUnspideredUrl(i) << std::endl;
 	}
 
@@ -79,18 +118,19 @@ void* short_term_scheduler(void* d){
 
 	for( auto e : url ){
 		crawl(e.first, e.second);
-		usleep(100000);
 	}
 
 	pthread_exit(NULL);
 }
 
+
 void long_term_scheduler(std::vector<std::string>& url){
+
+	int num_seeds=(int)url.size();
 	
 	std::map<std::string, std::vector<std::string>> host;
 
 	for( std::string s : url ){
-		all_links.push_back(s);
 		std::string d(domain(s));
 		host[d].push_back(s);
 	}
@@ -111,15 +151,30 @@ void long_term_scheduler(std::vector<std::string>& url){
 
 		int n = (int)host.size();
 		new_links.clear();
-		new_links.resize(n);
+		new_links.resize(num_seeds);
+			
+		qnt_crawled.clear();
+		qnt_crawled.resize(num_seeds, 0);
+		
+		size_html.clear();
+		size_html.resize(num_seeds, 0);
+		
+		time_crawling.clear();
+		time_crawling.resize(num_seeds, 0);
 		
 		std::vector<pthread_t> threads(n);
 
+		std::vector<std::string> websites;
 		int i=0;
+		int counter=0;
 		for( auto e : host ){
 			data* d = new data;
-			d->url = e.second;
-			d->id = i;
+		
+			for( std::string str : e.second ){
+				d->url.push_back({counter++, str});
+				websites.push_back(str);
+			}
+
 			int ret = pthread_create(&threads[i], NULL, short_term_scheduler, (void *)d);
 
 			if( ret ){
@@ -141,23 +196,33 @@ void long_term_scheduler(std::vector<std::string>& url){
 
 		if( debug ) std::cout << "Joined all Threads" << std::endl;
 
+		for( int i=0; i<num_seeds; i++ ){
+			to_output.push_back(statistics(websites[i], qnt_crawled[i], size_html[i], time_crawling[i]));
+			total_crawled += qnt_crawled[i];
+		}
+
 		host.clear();
-		for( i=0; i<n and not leave; i++ ){
+		num_seeds=0;
+		for( i=0; i<n; i++ ){
+			num_seeds += new_links.size();
 			for( std::string s : new_links[i] ){
-				all_links.push_back(s);
-				if( all_links.size() >= limit ){
-					leave = true;
-					break;
-				}
 				std::string d(domain(s));
 				host[d].push_back(s);
 			}
+		}
+
+		if( total_crawled >= limit ){
+			leave = true;
 		}
 	}
 }
 
 int main( int argc, char** argv ){
+		
+	clock_t begin = clock();
 
+	std::ios::sync_with_stdio(false);
+	
 	if( argc < 2 ){
 		std::cout << "Invalid parameters" << std::endl;
 		return 0;
@@ -179,7 +244,8 @@ int main( int argc, char** argv ){
 	}
 
 	if( debug ) std::cout << "Read input file" << std::endl;
-
+	
+	pthread_mutex_init(&mutex, NULL);
 	long_term_scheduler(url);
 
 	if( debug ) std::cout << "Done Crawling" << std::endl;
@@ -187,14 +253,21 @@ int main( int argc, char** argv ){
 	std::ofstream out("out.txt");
 
 	if( out.is_open() ){
-		for( auto s : all_links ){
-			out << s << std::endl;
+		for( auto s : to_output ){
+			out << s.website << std::endl;
+			out << "Number at level 1 = " <<  s.number << std::endl;
+			out << "Average size = " <<  s.average_size << std::endl;
+			out << "Average time = " <<  s.average_time << std::endl;
 		}
 		out.close();
 	}else{
 		std::cout << "Error: cant open write file" << std::endl;
 		return 0;
 	}
-
+	
+	clock_t end = clock();
+	
+	double total_time_elapsed = double(end-begin) / CLOCKS_PER_SEC;
+	std::cout << "Total time elapsed: " << total_time_elapsed << std::endl;
 
 }
